8fb68498b3d6d845ab0ab6dabc44ac1ab3998b3a
Zhahira <zhahira.s.r@mail.ugm.ac.id>
2020-03-24 02:15:13 +0700

    trying parent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
-import { Card, Col, Row } from 'antd';
 import { PageHeader } from 'antd';
-import { Statistic } from 'antd';
-import { LikeOutlined, DislikeOutlined } from '@ant-design/icons';
 import { Menu, Dropdown } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import CardName from './CardName/CardName';
 
 class App extends Component{
   state={
@@ -15,17 +12,12 @@ class App extends Component{
     totalsuccess: 0
   }
 
-  handleClickPositive = () => {
+  handleTotalChange = (newValue) => {
     this.setState({
-      total: this.state.total + 1,
-      totalsuccess: this.state.totalsuccess + 1
-    })
-  }
-
-  handleClickNegative = () => {
-    this.setState({
-      totalmin: this.state.totalmin + 1
-    })
+      total: newValue,
+      totalmin: newValue,
+      totalsuccess: newValue
+    });
   }
 
   render(){
@@ -48,47 +40,10 @@ class App extends Component{
         </Menu.Item>
       </Menu>
       ]
-    
-      const cardData = [
-        {
-          title:"Prawito",
-          job:"FrontEnd Dev",
-        },
-        {
-          title:"Handoko",
-          job:"Project Manager",
-        },
-        {
-          title:"Budi",
-          job:"UI Designer",
-        },
-        {
-          title:"Setia",
-          job:"UI Designer",
-        },
-        {
-          title:"Ani",
-          job:"Web Developer",
-        },
-        {
-          title:"Mari",
-          job:"IT Analyst",
-        },
-        {
-          title:"Prawito",
-          job:"FrontEnd Dev",
-        },
-        {
-          title:"Handoko",
-          job:"Project Manager",
-        },
-        {
-          title:"Budi",
-          job:"UI Designer",
-        }
-      ]
+  
     return (
-      [<div className="header-wrapper">
+      [
+      <div className="header-wrapper">
         <PageHeader
         className="site-page-header"
         onBack={() => null}
@@ -102,27 +57,9 @@ class App extends Component{
           </a>
         </React.Fragment>
       </Dropdown>
-      </div>
-      ,
-      
-      <div className="site-card-wrapper">
-      <Row gutter={16}>
-        {cardData.map(data =>
-          <Col span={8}>
-          <Card title="Nama Pegawai" bordered={false}>
-            
-            <p className="title">{data.title}</p>
-            <p className="subtitle">{data.job}</p>
-            <p><Button className="button" shape="round" icon={<LikeOutlined />} onClick={this.handleClickPositive}></Button>
-            <Button className="button" shape="round" icon={<DislikeOutlined />} onClick={this.handleClickNegative}></Button></p>
-            <p className="statistik"><Statistic title="Disukai" value={this.state.total} /></p>
-            <p className="statistik"><Statistic title="Tidak Disukai" value={this.state.totalmin} /></p>
-            <p className="statistika"><Statistic title="Statistik Keberhasilan" value={this.state.totalsuccess} suffix="/ 100" /></p>
-          </Card>
-          </Col>
-          )}
-      </Row>
-      </div>]
+      </div>,
+
+      <CardName onTotalChange={(value) => this.handleTotalChange(value)}/>]
     );
   }
 }
