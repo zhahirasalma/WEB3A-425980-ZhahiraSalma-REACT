01fb943051eef1c1e70014d5e30f73c36e8e9850
Zhahira <zhahira.s.r@mail.ugm.ac.id>
2020-03-29 19:43:11 +0700

    Updates

---
+++ precache-manifest.78834bff3836ea8bb074e77cbfd38a0d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82970292e784027aa2af13ad69a3e33",
+    "url": "/WEB3A-425980-ZhahiraSalma-REACT/index.html"
+  },
+  {
+    "revision": "25b3e67cf60c290fcc7a",
+    "url": "/WEB3A-425980-ZhahiraSalma-REACT/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "d7a49e8f0404aa93866b",
+    "url": "/WEB3A-425980-ZhahiraSalma-REACT/static/css/main.a3842324.chunk.css"
+  },
+  {
+    "revision": "25b3e67cf60c290fcc7a",
+    "url": "/WEB3A-425980-ZhahiraSalma-REACT/static/js/2.ee3ff289.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/WEB3A-425980-ZhahiraSalma-REACT/static/js/2.ee3ff289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a49e8f0404aa93866b",
+    "url": "/WEB3A-425980-ZhahiraSalma-REACT/static/js/main.1321f66d.chunk.js"
+  },
+  {
+    "revision": "a7ee82ab5276281204f7",
+    "url": "/WEB3A-425980-ZhahiraSalma-REACT/static/js/runtime-main.992140b2.js"
+  },
+  {
+    "revision": "1fef7c02124208f99bfe0bf575e8425f",
+    "url": "/WEB3A-425980-ZhahiraSalma-REACT/static/media/profil.1fef7c02.jpg"
+  },
+  {
+    "revision": "747f61d34ed786d8e590e5de3bb493d0",
+    "url": "/WEB3A-425980-ZhahiraSalma-REACT/static/media/profil2.747f61d3.jpg"
+  },
+  {
+    "revision": "7bda391715dc4b5b09b1ea87ffc70f67",
+    "url": "/WEB3A-425980-ZhahiraSalma-REACT/static/media/profil3.7bda3917.jpg"
+  }
+]);
